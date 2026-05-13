a1a53cb94d08dc38e614a6f49b99d6fdb50df0d2
hilemonmon <a12.5hjn@gmail.com>
2026-05-13 16:39:43 +0900

    Delete test/js directory

--- test/js/css-operation.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  // id属性がredの要素がクリックされたら
-  $('#red').on('click', function(){
-    // id属性がtargetの要素のcolorプロパティをredにする
-    $('#target').css('color', 'red');
-  });
- });
--- test/js/script.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  // ボタンがクリックされた場合
-  $('button').on('click', function(){
-    console.log('ボタンがクリックされました');
-  });
- });
